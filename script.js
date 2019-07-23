47b4694fa982d4346809113714123f949801a350
sombernightmare <timothytallen@gmail.com>
2019-07-22 23:55:42 -0500

    Working on bug, can’t see the task displayed. Wrote out the code for the clear completed task.

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list]')
 const newListInput = document.querySelector('[data-new-list-input]')
-const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 const deleteListBtn = document.querySelector('[data-delete-list-btn]')
 const listDisplay = document.querySelector('[data-list-display]')
 const listTitle = document.querySelector('[data-list-title]')
 const listCount = document.querySelector('[data-list-count]')
-const tasks = document.querySelector('[data-tasks]')
+const taskContainer = document.querySelector('[data-tasks]')
 const taskTemplate = document.querySelector('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
+const clearCompeteTaskBtn = document.querySelector('[data-clear-complete-tasks-btn]')
 
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) )|| []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
 
+clearCompeteTaskBtn.addEventListener('click', e => {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+    saveAndRender()
+})
+
+
+taskContainer.addEventListener('click', e =>{
+    if(e.target.tagName.toLowerCase() === 'input'){
+        const selectedList = lists.find(list => list.id === selectedListId)
+        const selectedTask =  selectedList.tasks.find(task => task.id === e.target.id)
+        selectedTask.complete = e.target.checked
+        save()
+        renderCount(selectedList)
+    }
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -26,6 +45,23 @@ newListForm.addEventListener('submit', e => {
     lists.push(list)
     saveAndRender()
 })
+
+
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if(taskName == null || taskName === '') return
+
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender()
+})
+
+function createTask(name) {
+    return {id: Date.now().toString(), name: name, complete: false }
+}
 
 listsContainer.addEventListener('click', e=>{
     if (e.target.tagName.toLowerCase() === 'li'){
@@ -51,11 +87,7 @@ function save(){
 }
 
 function createList(name){
-    return {id: Date.now().toString(), name: name, tasks:[{
-        id: '233342',
-        name: 'test',
-        complete: false
-    }]}
+    return {id: Date.now().toString(), name: name, tasks:[]}
 }
 
 function render(){   
@@ -68,7 +100,7 @@ function render(){
         listDisplay.style.display = ''
         listTitle.innerText = selectedList.name
         renderCount(selectedList)
-        clearElement(tasks)
+        clearElement(taskContainer)
         renderTasks(selectedList)
     }
 }
@@ -93,7 +125,7 @@ function renderLists() {
 }
 
 function renderTasks(selectedList){
-    selectedList.tasks.forEach(task =>{
+    selectedList.taskContainer.forEach(task =>{
         const taskElement = document.importNode(taskTemplate.content, true)
         const checkbox = taskElement.querySelector('input')
         checkbox.id = task.id
@@ -102,7 +134,7 @@ function renderTasks(selectedList){
 
         label.htmlFor = task.id
         label.append(task.name)
-        tasks.appendChild(taskElement)
+        taskContainer.appendChild(taskElement)
 
     })
 }
